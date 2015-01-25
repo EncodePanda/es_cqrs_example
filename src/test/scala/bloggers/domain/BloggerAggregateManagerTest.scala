0fa08604ecabdbda5b05278237a12a45e153c8da
Paweł Szulc <pawel.szulc@blstream.com>
2015-01-25 16:37:23 +0100

    introduce helper method for aggregate tests

--- src/test/scala/bloggers/domain/BloggerAggregateManagerTest.scala
+++ src/test/scala/bloggers/domain/BloggerAggregateManagerTest.scala
@@ -1,8 +1,9 @@
 package bloggers.domain
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
 import akka.testkit.TestActorRef
 import akka.util.Timeout
+import bloggers.domain.AggregateRoot.Command
 import scala.concurrent.duration._
 import scala.concurrent.{Future, Await}
 import org.scalatest.{Matchers, BeforeAndAfterAll, FunSuite}
@@ -36,14 +37,18 @@ class BloggerAggregateManagerTest extends FunSuite with Matchers with BeforeAndA
 
   test("that aggregate is initialized with initial state") {
     // given
-    val manager = TestActorRef(BloggerAggregateManager.props)
+    implicit val manager = TestActorRef(BloggerAggregateManager.props)
     // when
-    val future = (manager ? Initialize("paul", "szulc")).mapTo[Blogger]
-    val blogger = Await.result(future, 2 seconds)
-    // then
-    blogger match {
+    commanded(Initialize("paul", "szulc")) {
+      // then
       case Blogger(_, "paul", "szulc") =>
       case sthElse => fail("not a blogger we've expected, got " + sthElse)
     }
+  }
+
+  private def commanded(command: Command)(after: Blogger => Any)(implicit manager: ActorRef): Unit = {
+    val future = (manager ? command).mapTo[Blogger]
+    val blogger = Await.result(future, 2 seconds)
+    after(blogger)
   }
 }
